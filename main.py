773e9c38eb37c263c95258db7d180abf4419a339
KevalVora09 <94117309+KevalVora09@users.noreply.github.com>
2024-09-23 20:53:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ st.set_page_config(page_title='IPL WIN PREDICTOR')
 
 hide_badge_css = """
 <style>
-.viewerBadge_container__r5tak styles_viewerBadge__CvC9N {
+.viewerBadge_text__fzr3E viewerBadge_te
+xtExi__9s6DV viewerBadge_textExitActive__KVw5r
+ {
     display: none !important;
 }
 </style>
