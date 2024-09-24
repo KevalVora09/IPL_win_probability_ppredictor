6d6442d4775d30550488027a697e67284f322314
KevalVora09 <94117309+KevalVora09@users.noreply.github.com>
2024-09-24 18:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import pandas as pd
 
 st.set_page_config(page_title='IPL WIN PREDICTOR')
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.markdown("""
 <style>
