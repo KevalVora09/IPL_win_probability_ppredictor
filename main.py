9d321a4a7de8a90668921304a9fe5697f37b98fb
KevalVora09 <94117309+KevalVora09@users.noreply.github.com>
2025-04-10 23:34:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-st.set_page_config(page_title='IPL WIN PREDICTOR')
+st.set_page_config(page_title='IPL WIN PREDICTORRR')
 st.markdown(':red[**(Made by - Keval Vora)**]')
 
 st.markdown("""
